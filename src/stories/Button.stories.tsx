72f4a099cf95692950c92dba2d2de1cd78f27703
Torkel Rogstad <torkel@rogstad.io>
2020-10-08 13:38:22 +0200

    Add storybook with css pseudo states

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { withPseudo } from '@ergosign/storybook-addon-pseudo-states-react';
+
+import { Button, ButtonProps } from './Button';
+import styles from "../styling.scss"
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
+
+export const styling: Story = () => <div>{JSON.stringify(styles)}</div>
